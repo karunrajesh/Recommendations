dbe7e267bc9182014104c517e8de26070fb2a440
karunrajesh <rajeshk@vcu.edu>
2021-01-14 19:27:58 -0500

    Added subsetting

--- app.py
+++ app.py
@@ -8,10 +8,7 @@ import pandas as pd
 import pickle
 import networkx as nx
 
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 with open('ani_recs.pickle', 'rb') as handle:
     ani_recs = pickle.load(handle)
@@ -24,10 +21,17 @@ df = pd.DataFrame({
     'AniList': animes
 })
 
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-layout = html.Div([
-      visdcc.Network(id = 'net',
-                     options = dict(height= '600px', width= '100%')),
+app.layout = html.Div([
+      visdcc.Network(id = 'net', options = dict(height= '600px',
+                                                width= '100%',
+                                                physics = {"forceAtlas2Based": {"springLength": 100},
+                                                           "minVelocity": 0.75,"solver": "forceAtlas2Based"},
+                                                interaction = {"hover": True,
+                                                                "keyboard": {"enabled": True},
+                                                               "navigationButtons": True}
+)),
       dcc.Dropdown(id = "Dropdown",
                    options=[
                        {'label': i, 'value': i} for i in df['AniList'].unique()
@@ -40,21 +44,28 @@ layout = html.Div([
     Output('net', 'data'),
     [Input('Dropdown', 'value')])
 def myfun(x):
+    df_func = pd.DataFrame({
+        'AniList': list(g.nodes())
+    })
+
     if(x == "All"):
-        return g
+        data = {'nodes': [{'id': i, 'label': i} for i in df_func['AniList'].unique()],
+                'edges': [{'id': str(edge[0]) + "-" + str(edge[1]), 'from': edge[0], 'to': edge[1]} for edge in
+                          g.edges()]
+                }
+        return data
     node_list = set(x)
     for neighbor in g.neighbors(x):
         node_list.add(neighbor)
         for next_neighbor in g.neighbors(neighbor):
             node_list.add(next_neighbor)
     sub_g = nx.subgraph(g, node_list)
-    return sub_g
-"""
-@app.callback(
-    Output('net', 'options'),
-    [Input('color', 'value')])
-def myfun(x):
-    return {'nodes':{'color': x}}
-"""
+    data = {'nodes': [{'id': i, 'label': i} for i in list(sub_g.nodes())],
+            'edges': [{'id': str(edge[0]) + "-" + str(edge[1]), 'from': edge[0], 'to': edge[1]} for edge in
+                      sub_g.edges()]
+            }
+    return data
+
 if __name__ == '__main__':
     app.run_server(debug=True)
+
